e5a3df72ce0758a7202ddf0e7235a0c0f69121ec
2004wan <wyr666@student.ubc.ca>
2025-12-09 12:09:16 -0800

    test_suite finished

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -10,3 +10,18 @@ from test_question import TestQuestion
 from test_question_manager import TestQuestionManager
 from test_user import TestUserClasses
 from test_user_manager import TestUserManager
+
+def suite():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+    suite.addTests(loader.loadTestsFromTestCase(TestQuizSession))
+    suite.addTests(loader.loadTestsFromTestCase(TestQuizResult))
+    suite.addTests(loader.loadTestsFromTestCase(TestQuestion))
+    suite.addTests(loader.loadTestsFromTestCase(TestQuestionManager))
+    suite.addTests(loader.loadTestsFromTestCase(TestUserClasses))
+    suite.addTests(loader.loadTestsFromTestCase(TestUserManager))
+    return suite
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite())
